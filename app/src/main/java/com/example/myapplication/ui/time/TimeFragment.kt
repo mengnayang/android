5317d7341734f81e8e3c3bed1158887cf6d8d69e
yangmengna <2235749686@qq.com>
2020-12-22 20:57:06 +0800

    '完成计时器功能的转换'

--- app/src/main/java/com/example/myapplication/ui/time/TimeFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/time/TimeFragment.kt
@@ -2,19 +2,26 @@ package com.example.myapplication.ui.time
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.view.Display
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.Observer
 import com.example.myapplication.R
+import kotlinx.android.synthetic.main.fragment_time.*
 
 class TimeFragment : Fragment() {
+
+    //获取对应的viewModel
+    lateinit var viewModel: TimeViewModel
 
     companion object {
         fun newInstance() = TimeFragment()
     }
 
-    private lateinit var viewModel: TimeViewModel
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,10 +30,36 @@ class TimeFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_time, container, false)
     }
 
+    //此时Activity, viewModel均有
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(TimeViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
 
+        //监听viewModel中second的变化
+        viewModel.second.observe(viewLifecycleOwner, Observer {
+            val hours = it / 3600
+            val minute = it / 60
+            val second = it % 60
+            textView_time.text = String.format("%02d:%02d:%02d", hours, minute, second)
+        })
+
+        //调用viewModel中的开始按钮
+        time_btn_start.setOnClickListener {
+            viewModel.start()
+            //此时不能用this, 因为此刻不处于Activity中, 需要用activity才行
+            Toast.makeText(activity, "计时开始", Toast.LENGTH_SHORT).show()
+        }
+
+        //调用viewModel中的停止按钮
+        time_btn_stop.setOnClickListener {
+            viewModel.stop()
+            Toast.makeText(activity, "计时停止", Toast.LENGTH_SHORT).show()
+        }
+
+        //调用viewModel中的重置按钮
+        time_btn_reset.setOnClickListener {
+            viewModel.reset()
+            Toast.makeText(activity, "计时重置", Toast.LENGTH_SHORT).show()
+        }
+    }
 }
--- app/src/main/java/com/example/myapplication/ui/time/TimeViewModel.kt
+++ app/src/main/java/com/example/myapplication/ui/time/TimeViewModel.kt
@@ -1,7 +1,53 @@
 package com.example.myapplication.ui.time
 
+import android.os.Handler
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
+
 class TimeViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    //内部变量
+    private var _second:MutableLiveData<Int> = MutableLiveData()
+    private var running:Boolean = false
+
+    //供外部监听访问的对象
+    var second:LiveData<Int> = _second
+
+    //初始化定时器
+    init {
+        runTimer()
+    }
+
+    //开始计数
+    fun start() {
+        running = true
+    }
+
+    //停止计时
+    fun stop() {
+        running = false
+    }
+
+    //重置计时结果
+    fun reset() {
+        running = false
+        _second.value = 0
+    }
+
+    fun runTimer() {
+        val handler = Handler()
+        val runnable = object :Runnable {
+            override fun run() {
+                if (running) {
+                    // 主线程可以用.value获取, 子线程需要用postValue获取
+                    // 判断_second.value是否为空, 为空取0
+                    val temp = _second.value ?: 0
+                    _second.value = temp + 1
+                }
+                handler.postDelayed(this, 1000)
+            }
+        }
+        handler.post(runnable)
+    }
 }
