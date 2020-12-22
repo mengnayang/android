efff8ae920d4f58b7e64cb770d71e1201700c773
yangmengna <2235749686@qq.com>
2020-12-22 19:22:13 +0800

    '计时器底部导航栏'

---
+++ app/src/main/java/com/example/myapplication/ui/time/TimeFragment.kt
@@ -0,0 +1,32 @@
+package com.example.myapplication.ui.time
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.myapplication.R
+
+class TimeFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = TimeFragment()
+    }
+
+    private lateinit var viewModel: TimeViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_time, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(TimeViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/myapplication/ui/time/TimeViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.myapplication.ui.time
+
+import androidx.lifecycle.ViewModel
+
+class TimeViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
